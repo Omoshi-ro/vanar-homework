ea8cbef3d2390adbbff539b9ae44706c1d1687c9
Omoshi-ro <125890042+Omoshi-ro@users.noreply.github.com>
2023-02-20 15:30:42 +0200

    Add files via upload

---
+++ exercițiul_nr.10.py
@@ -0,0 +1,11 @@
+from os import system
+system("CLS")
+
+while True:
+    print("BOO YA!")
+    answer = input("Do you wan't me to stop? (yes or no) ")
+    if answer == "yes":
+        break
+    else:
+        print("Will be infinite loop")
+        break
