472cbaaf41605879feaf08978bfe41c62e4e6853
Omoshi-ro <125890042+Omoshi-ro@users.noreply.github.com>
2023-02-25 21:14:34 +0200

    Add files via upload

--- exercițiul_nr.11.py
+++ exercițiul_nr.11.py
@@ -1,9 +1,13 @@
 from os import system
 system("CLS")
-
 big_ship = int(input("Enter an number: (1 till 10) "))
+
 for x in range(1,11):
-    if x == big_ship:
-      print( "wWw", end="" )
+    if big_ship == x:
+      print("W", end="")
+    elif x == big_ship -1:
+      print("w", end="")
+    elif x == big_ship +1:
+      print("w", end="")
     else:
-      print( "~", end="" )
+      print( "~", end="")
